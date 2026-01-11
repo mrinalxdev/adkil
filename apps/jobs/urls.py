0c0e3c9eb49b26ed3575364fdc6c0d0806edbf56
Mrinal Pramanick <mrinalbitsat@gmail.com>
2026-01-11 13:50:08 +0530

    Added scheduled cleanup

--- apps/jobs/urls.py
+++ apps/jobs/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.JobCreateView.as_view(), name='create-job'),
     path('<int:pk>/', views.JobDetailView.as_view(), name='job-detail'),
+    path('cleanup/', views.CleanupArtifactsView.as_view(), name='cleanup-artifacts')
 ]
